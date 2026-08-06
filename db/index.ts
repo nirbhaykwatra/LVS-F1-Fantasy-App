21f68f0c6ad096e7ba68b029819e1d70c3f2d419
Nirbhay Kwatra <kwatranirbhay@gmail.com>
2026-08-06 02:14:36 -0700

    Pass relations into Drizzle as well as schema

--- db/index.ts
+++ db/index.ts
@@ -1,4 +1,5 @@
 ﻿import { drizzle as drizzlePostgres } from 'drizzle-orm/node-postgres'
 import * as schema from './schema';
+import * as relations from './relations';
 
-export const db = drizzlePostgres(process.env.DATABASE_URL!, { schema, casing: 'snake_case' });
+export const db = drizzlePostgres(process.env.DATABASE_URL!, { schema: { ...schema , ...relations}, casing: 'snake_case' });
